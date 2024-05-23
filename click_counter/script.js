a881c8a3d8deb8e71f5549fe032ec1e800622326
Ankit Gill <166650090+ankitgill07@users.noreply.github.com>
2024-05-23 22:23:57 +0530

    Update script.js

--- click_counter/script.js
+++ click_counter/script.js
@@ -20,3 +20,37 @@ window.onload = function () {
   function updateDisplay(val) {
     document.getElementById("counter").innerHTML = val;
   }  
+
+
+//this is my code
+
+window.onload = function () {
+    let clickSection = document.getElementById("click");
+    clickSection.onclick = addClick;
+
+    let resetButton = document.getElementById("reset");
+    resetButton,onclick = resetCounter;
+};
+
+var counterValue = 0;
+
+addClick = function () {
+    updateDisplay(++counterValue);
+};
+
+function resetCounter(){
+    counterValue = 0;
+    updateDisplay(counterValue);
+};
+
+function updateDisplay(val){
+    document.getElementById("counter").innerHTML = val;
+};
+
+
+
+
+
+
+
+
